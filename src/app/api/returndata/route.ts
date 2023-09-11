bce2fa55f7d1b9c7c1a86f2f796a1e1e248e1a34
javadng <javadnasirifu@gmail.com>
2023-09-11 13:28:38 +0330

    master:

--- src/app/api/returndata/route.ts
+++ src/app/api/returndata/route.ts
@@ -2,23 +2,29 @@ import { NextResponse } from "next/server";
 import EnterUserModel from "../../../../models/enterModel";
 import connectDB from "@/utils/db";
 
+// export async function POST(request: Request) {
+//   try {
+//     const isConnected = await connectDB();
+
+//     const response = await request.json();
+
+//     if (!isConnected)
+//       return NextResponse.json({ message: "fail to connect mongoDB" });
+
+//     const { id } = response;
+//     const date = new Date();
+
+//     await EnterUserModel.create({ uid: id, date });
+
+//     return NextResponse.json({ id, date });
+//   } catch (error: any) {
+//     console.log(error);
+//     return NextResponse.json({ status: "fail", message: error.message });
+//   }
+// }
+
 export async function POST(request: Request) {
-  try {
-    const isConnected = await connectDB();
-
-    const response = await request.json();
-
-    if (!isConnected)
-      return NextResponse.json({ message: "fail to connect mongoDB" });
-
-    const { id } = response;
-    const date = new Date();
-
-    await EnterUserModel.create({ uid: id, date });
-
-    return NextResponse.json({ id, date });
-  } catch (error: any) {
-    console.log(error);
-    return NextResponse.json({ status: "fail", message: error.message });
-  }
+  const response = await request.json();
+  const { id } = response;
+  return NextResponse.json({ id });
 }
