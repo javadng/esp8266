a15af88bf0f85872b070cfe163d919d53cfd9896
javadng <javadnasirifu@gmail.com>
2023-10-13 19:34:29 +0330

    master: api route : get-activity => return just today log

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,19 +1,17 @@
-import { DBURL } from "@/utils/db";
 import formatDate from "@/utils/formatDate";
-import mongoose, { ObjectId } from "mongoose";
-import EnterUserModel from "../../../models/enterModel";
+import { ObjectId } from "mongoose";
 
 interface LogItem {
-  enter: String;
+  enter: Date;
   formatedDate: String;
-  exit: String;
+  exit: Date;
+  _id: ObjectId;
 }
 
 interface UserLogItem {
-  _id: ObjectId;
   uid: String;
   username: String;
-  userLogins: LogItem[];
+  logs: LogItem[];
 }
 
 const ActivityPage = async () => {
@@ -36,23 +34,23 @@ const ActivityPage = async () => {
   }
 
   if (allActivity && allActivity?.length > 0) {
-    elements = allActivity.map((item: any) => (
+    elements = allActivity.map((el) => (
       <div
-        key={item.uid}
+        key={Math.random()}
         className="shadow-lg my-6 w-3/4 mx-auto rounded-3xl bg-indigo-50 p-3 flex flex-col justify-evenly items-center"
       >
         <h2 className="text-start self-start ml-10">
           <span className="italic">Username : </span>
-          <span className="font-bold">{item.username}</span>
+          <span className="font-bold">{el.username}</span>
         </h2>
         <ul className="grid grid-col-1">
-          {item.userLogins.length > 0 &&
-            item.userLogins.map((item: any) => {
+          {el.logs.length > 0 &&
+            el.logs.map((item) => {
               const enterDate = new Date(item.enter);
               const exitDate = item.exit ? new Date(item.exit) : null;
               return (
                 <li
-                  key={item._id}
+                  key={Math.random()}
                   className="grid md:grid-cols-2 gap-3 justify-between my-1 odd:bg-indigo-400 even:bg-indigo-100 odd:text-white p-3 rounded-2xl"
                 >
                   <h3 className="row-start-2 sm:row-start-1">
@@ -67,7 +65,7 @@ const ActivityPage = async () => {
               );
             })}
 
-          {item.userLogins.length <= 0 && (
+          {el.logs.length <= 0 && (
             <span className="block underline italic text-red-400">
               No logs Yet
             </span>
--- src/app/api/get-activity/route.ts
+++ src/app/api/get-activity/route.ts
@@ -1,17 +1,49 @@
 import DBConnectHandler from "@/utils/dbConnectFn";
 import { NextRequest, NextResponse } from "next/server";
 import EnterUserModel from "../../../../models/enterModel";
+import { ObjectId } from "mongoose";
+
+interface LogItem {
+  enter: String;
+  formatedDate: String;
+  exit: String;
+}
+
+interface UserLogItem {
+  _id: ObjectId;
+  uid: String;
+  username: String;
+  userLogins: LogItem[];
+}
 
 export async function GET(req: NextRequest, res: NextResponse) {
   await DBConnectHandler(NextResponse);
 
   try {
-    const allUsersAcivity = await EnterUserModel.find({});
+    const allUsersAcivity: UserLogItem[] = await EnterUserModel.find({});
+
+    const todayDate = new Date();
+    const todayDateFormated = Intl.DateTimeFormat("fa-IR", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(todayDate);
+
+    const todayLogins = allUsersAcivity.map((el) => {
+      const todayLogs = el.userLogins.filter(
+        (log) => log.formatedDate === todayDateFormated
+      );
+
+      if (todayLogs.length) {
+        return { uid: el.uid, username: el.username, logs: todayLogs };
+      } else return null;
+    });
+    const todayLoginsFilterd = todayLogins.filter((el) => el !== null);
 
     return NextResponse.json({
       status: "success",
       message: "",
-      data: allUsersAcivity,
+      data: todayLoginsFilterd,
     });
   } catch (error: any) {
     return NextResponse.json({
